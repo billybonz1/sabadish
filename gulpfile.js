ea2bf9ae48048353d442983e7855d2d96e634f98
Artem Kalinichenko <artem21kalinichenko@gmail.com>
2016-06-13 16:08:45 +0300

    First

--- gulpfile.js
+++ gulpfile.js
@@ -1,50 +1,64 @@
-var gulp         = require('gulp'),
-		sass         = require('gulp-sass'),
-		autoprefixer = require('gulp-autoprefixer'),
-		cleanCSS    = require('gulp-clean-css'),
-		rename       = require('gulp-rename'),
-		browserSync  = require('browser-sync').create(),
-		concat       = require('gulp-concat'),
-		uglify       = require('gulp-uglify');
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    cleanCSS = require('gulp-clean-css'),
+    rename = require('gulp-rename'),
+    browserSync = require('browser-sync').create(),
+    concat = require('gulp-concat'),
+    otf2ttf = require('otf2ttf'),
+    uglify = require('gulp-uglify'),
+    inject = require('gulp-inject');
 
-gulp.task('browser-sync', ['styles', 'scripts'], function() {
-		browserSync.init({
-				server: {
-						baseDir: "./app"
-				},
-				notify: false
-		});
+gulp.task('browser-sync', ['styles', 'scripts'], function () {
+    browserSync.init({
+        server: {
+            baseDir: "./app"
+        },
+        notify: false
+    });
 });
 
 gulp.task('styles', function () {
-	return gulp.src('sass/*.sass')
-	.pipe(sass({
-		includePaths: require('node-bourbon').includePaths
-	}).on('error', sass.logError))
-	.pipe(rename({suffix: '.min', prefix : ''}))
-	.pipe(autoprefixer({browsers: ['last 15 versions'], cascade: false}))
-	.pipe(cleanCSS())
-	.pipe(gulp.dest('app/css'))
-	.pipe(browserSync.stream());
+    return gulp.src('sass/*.sass')
+        .pipe(sass({
+            includePaths: require('node-bourbon').includePaths
+        }).on('error', sass.logError))
+        .pipe(rename({suffix: '.min', prefix: ''}))
+        .pipe(autoprefixer({browsers: ['last 15 versions'], cascade: false}))
+        .pipe(cleanCSS())
+        .pipe(gulp.dest('app/css'))
+        .pipe(browserSync.stream());
 });
 
-gulp.task('scripts', function() {
-	return gulp.src([
-		'./app/libs/modernizr/modernizr.js',
-		'./app/libs/jquery/jquery-1.11.2.min.js',
-		'./app/libs/waypoints/waypoints.min.js',
-		'./app/libs/animate/animate-css.js',
-		])
-		.pipe(concat('libs.js'))
-		// .pipe(uglify()) //Minify libs.js
-		.pipe(gulp.dest('./app/js/'));
+gulp.task('scripts', function () {
+    return gulp.src([
+        './app/libs/modernizr/modernizr.js',
+        './app/libs/jquery/jquery-1.11.2.min.js',
+        './app/js/common.js',
+        // './app/libs/waypoints/waypoints.min.js',
+        // './app/libs/animate/animate-css.js',
+    ])
+        .pipe(concat('libs.js'))
+        // .pipe(uglify()) //Minify libs.js
+        .pipe(gulp.dest('./app/js/'));
+});
+
+
+gulp.task('index', function () {
+    var target = gulp.src('app/index.html');
+    // It's not necessary to read the files (will speed up things), we're only after their paths:
+    var sources = gulp.src(['app/js/libs.js', 'app/css/main.min.css'], {read: false});
+
+    return target.pipe(inject(sources))
+        .pipe(gulp.dest('app'));
 });
 
 gulp.task('watch', function () {
-	gulp.watch('sass/*.sass', ['styles']);
-	gulp.watch('app/libs/**/*.js', ['scripts']);
-	gulp.watch('app/js/*.js').on("change", browserSync.reload);
-	gulp.watch('app/*.html').on('change', browserSync.reload);
+    gulp.watch('sass/*.sass', ['styles']);
+    gulp.watch('app/libs/**/*.js', ['scripts']);
+    gulp.watch('app/js/common.js', ['scripts']);
+    gulp.watch('app/js/*.js').on("change", browserSync.reload);
+    gulp.watch('app/*.html').on('change', browserSync.reload);
 });
 
 gulp.task('default', ['browser-sync', 'watch']);
